04c5b53e0bc3e08da13f6b275480fafd76178ba1
Muhammad Zuhdi Zamrud <mzuhdi.zamrud@gmail.com>
2020-04-14 05:21:13 +0700

    A little change in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /run/media/zuhdi/WIN_D/Backup_Zuhdi/Work/BEM/Bismit_2020/Projects/Asji/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /run/media/zuhdi/WIN_D/Backup_Zuhdi/Work/BEM/Bismit_2020/Projects/Asji/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /run/media/zuhdi/WIN_D/Backup_Zuhdi/Work/BEM/Bismit_2020/Projects/Asji_Repo_Data/Asji/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /run/media/zuhdi/WIN_D/Backup_Zuhdi/Work/BEM/Bismit_2020/Projects/Asji_Repo_Data/Asji/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
